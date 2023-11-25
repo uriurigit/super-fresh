7926915198c18523afb89a37a22217184234f3be
uriuri <uriuri7610@gmail.com>
2023-11-25 19:04:47 +0900

    second commit

--- routes/detail.tsx
+++ routes/detail.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        詳細！
+        ここに詳細を書きます。
       </div>
     </div>
   );
