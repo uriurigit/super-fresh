a8e76506748ca70b7650e80f8ef99d03f05697fa
uriuri <uriuri7610@gmail.com>
2023-11-25 18:57:01 +0900

    init commit

---
+++ routes/detail.tsx
@@ -0,0 +1,11 @@
+import { useSignal } from "@preact/signals";
+
+export default function Home() {
+  return (
+    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        詳細！
+      </div>
+    </div>
+  );
+}
